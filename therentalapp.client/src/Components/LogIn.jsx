a6f970679c6c81bcf433c5a70f97bf5926e277fd
Madelene Axblom <90625377+GreenGard@users.noreply.github.com>
2024-08-27 21:14:33 +0200

    Add simple login form just frontend

--- therentalapp.client/src/Components/LogIn.jsx
+++ therentalapp.client/src/Components/LogIn.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 function LogIn() {
     return (
-        <div>
-            <h1>add template</h1>
-       
+        <div class="login-container">
+            <h2>Logga in</h2>
+            <form action="/login" method="post">
+                <input type="text" name="username" placeholder="Användarnamn" required/>   
+                <input type="password" name="password" placeholder="Lösenord" required/>
+                <input type="submit" value="Logga in"/>
+                        </form>
+                        <button class="google-btn">Logga in med Google</button>
+                        <a class="signup-link" href="/signup">Skapa ett konto</a>
         </div>
     );
 }
