a4009cd4ec6107dc7357fda3b259907727d7c7cc
Madelene Axblom <90625377+GreenGard@users.noreply.github.com>
2024-12-04 22:29:23 +0100

    added constructor

--- TheRentalApp.Server/Models/User.cs
+++ TheRentalApp.Server/Models/User.cs
@@ -5,6 +5,11 @@
         public int Id { get; set; }
         public required string Username { get; set; }
         public required string PasswordHash { get; set; }
-      //  public ICollection<UserRole> UserRoles { get; set; }
+        //  public ICollection<UserRole> UserRoles { get; set; }
+        public User(string username, string passwordHash)
+        {
+            Username = username;
+            PasswordHash = passwordHash;
+        }
     }
 }
